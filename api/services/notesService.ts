6807b4dc9190737abf4458700dbae8ccfd8b9ab9
batmanastasiya <batmanastasiyasergeevna@gmail.com>
2023-11-27 15:10:38 +0200

    fix: adding test failing in tests with try/catch, using axios response interface instead not default one, fix readme

--- api/services/notesService.ts
+++ api/services/notesService.ts
@@ -1,5 +1,6 @@
+import { AxiosResponse } from 'axios';
 import { ApiClient } from '../apiClient';
-import { IApiResponse, INote } from '../IResponses.interface';
+import { INote } from '../IResponses.interface';
 import { ICreateNoteRequest } from '../IRequests.interface';
 
 export class NotesService {
@@ -7,30 +8,30 @@ export class NotesService {
     this.client = client;
   }
 
-  async getNotes(): Promise<IApiResponse<INote[]>> {
+  async getNotes(): Promise<AxiosResponse<INote[]>> {
     return this.client.get('/notes');
   }
 
-  async createNote(data: ICreateNoteRequest): Promise<IApiResponse<INote>> {
+  async createNote(data: ICreateNoteRequest): Promise<AxiosResponse<INote>> {
     return this.client.post('/notes', data);
   }
 
-  async getNoteById(id: string): Promise<IApiResponse<INote>> {
+  async getNoteById(id: string): Promise<AxiosResponse<INote>> {
     return this.client.get(`/notes/${id}`);
   }
 
   async updateNoteById(
     id: string,
     data: ICreateNoteRequest,
-  ): Promise<IApiResponse<INote>> {
+  ): Promise<AxiosResponse<INote>> {
     return this.client.patch(`/notes/${id}`, data);
   }
 
-  async deleteNoteById(id: string): Promise<IApiResponse> {
+  async deleteNoteById(id: string): Promise<AxiosResponse> {
     return this.client.delete(`/notes/${id}`);
   }
 
-  async getNotesByUserId(id: string): Promise<IApiResponse<INote[]>> {
+  async getNotesByUserId(id: string): Promise<AxiosResponse<INote[]>> {
     return this.client.get(`/notes/author/${id}`);
   }
 }
